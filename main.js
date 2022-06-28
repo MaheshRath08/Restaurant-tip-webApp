fa2a5652540977f32ea6f711f34bb910bf1b83ae
MaheshRath08 <maheshrath.2002@gmail.com>
2022-06-28 15:39:27 +0530

    fixed calculation errors

--- main.js
+++ main.js
@@ -6,11 +6,11 @@ let popup = document.getElementById("popup")
 let ans = document.getElementById("h1")
 
 submit.addEventListener("click", function(){
-    let ga = grossAmount.value
-    let gst = gstEl.value
-    let tip = tipEl.value
+    let ga = parseFloat(grossAmount.value)
+    let gst = parseFloat(gstEl.value)
+    let tip = parseFloat(tipEl.value)
     let netAmount = ga + ga*(gst/100) + tip
     popup.style.transform = "scale(1)"
-    ans.innerHTML = `Net Amount is ${netAmount}`  
+    ans.innerHTML = `Net Amount is ${netAmount}₹`  
     popup.innerHTML += `<br><button onclick="location.reload()">Reset</button>`
 })
