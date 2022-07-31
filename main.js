0d25449a5afd7486ef443305129a6dd7225ff113
MaheshRath08 <maheshrath.2002@gmail.com>
2022-07-31 22:52:49 +0530

    made other values visible on the popup

--- main.js
+++ main.js
@@ -10,13 +10,14 @@ submit.addEventListener("click", function(){
     let ga = parseFloat(grossAmount.value)
     let gst = parseFloat(gstEl.value)
     let tip = parseFloat(tipEl.value)
-    let netAmount = ga + ga*(gst/100) + tip
+    let gstCalculated = ga*(gst/100)
+    let netAmount = ga + gstCalculated + tip
     if (isNaN(netAmount)){
         alert("PLEASE ADD VALID VALUES!!!!")
         location.reload()
     }else{
         popup.style.transform = "scale(1)"
-        ans.innerHTML = `Net Amount is ${netAmount}₹`  
+        ans.innerHTML = `Net Amount is ${netAmount}₹<br>GST is ${gstCalculated}₹<br> Tip is ${tip}₹`  
         containerEl.style.transform = "scale(0)"
         popup.innerHTML += `<br><button onclick="location.reload()">Reset</button>`
     }
